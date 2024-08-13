b9a0e33b0f295fbbb4a8e82dec53823f5e1e43ee
kachunchan-analytics <kachunchan.a@gmail.com>
2024-08-13 11:04:19 +0800

    Updated new prompts

---
+++ PUSH_TO_GITHUB.bat
@@ -0,0 +1,7 @@
+@echo off
+cd /d "C:\Users\Andy\Desktop\python_work_github\prompt_engineering_GITHUB\prompt_engineering_collection"
+rem cd /d %cd%
+
+git add . 
+git commit -m "Updated new prompts"
+git push
